67a973a9cc3e560fe29c38952f4d28412b5ca550
efezino <enduranceidisi13@gmail.com>
2024-12-15 16:56:45 +0100

    update sum functions to return correct type

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -11,13 +11,19 @@ interface MinorCredits {
 function sumMajorCredits(
   subject1: MajorCredits,
   subject2: MajorCredits
-): number {
-  return subject1.credits + subject2.credits;
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MajorCredits',
+  };
 }
 
 function sumMinorCredits(
   subject1: MinorCredits,
   subject2: MinorCredits
-): number {
-  return subject1.credits + subject2.credits;
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MinorCredits',
+  };
 }
